1b80a4e636f67d0af6e2da65cc4c40113f6762d8
Wesley Gonçalves Luciano <47697477+wesleylg01@users.noreply.github.com>
2019-04-13 20:17:41 -0300

    Add files via upload

---
+++ DB/DELIVERY.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE DELIVERY
+GO
+
+USE DELIVERY
+
+CREATE TABLE EMPRESA (IdEmpresa		INT PRIMARY KEY,
+                      Nome_Fantasia VARCHAR(100),
+					  Razao_Social  VARCHAR(100),
+					  CNPJ			VARCHAR(18),
+					  email			VARCHAR(50),
+					  Telefone		VARCHAR(11),
+					  CEP			VARCHAR(9),
+					  Endereco		VARCHAR(255),
+					  UF			VARCHAR(2),
+					  Num_Endereco  INT,
+					  Bairro		VARCHAR(50),
+					  Cidade		VARCHAR(50),
+					  Complemento	VARCHAR(255))
+
+CREATE TABLE FUNCIONARIO (IdFuncionario	INT PRIMARY KEY,
+                          CPF			VARCHAR(14),
+						  RG			VARCHAR(12),
+						  Bairro		VARCHAR(50),
+						  Telefone		VARCHAR(11),
+						  Data_Nasc		VARCHAR(8),
+						  CEP		    VARCHAR(9),
+						  Endereco	    VARCHAR(255),
+						  Num_Endereco  INT,
+						  Cidade		VARCHAR(50),
+						  UF			VARCHAR(2),
+						  Complemento	VARCHAR(255),
+						  Nome			VARCHAR(50),
+						  Email			VARCHAR(50),
+						  Ativo			VARCHAR(1),
+						  IdEmpresa     INT FOREIGN KEY REFERENCES EMPRESA(IdEmpresa) NOT NULL)
+
+CREATE TABLE USUARIO (IdUsuario     INT PRIMARY KEY,
+                      IdFuncionario INT FOREIGN KEY REFERENCES FUNCIONARIO(IdFuncionario),
+					  Usuario       VARCHAR(16),
+					  Senha         VARCHAR(16))
+
+CREATE TABLE CLIENTE (IdCliente		INT PRIMARY KEY,
+                      CPF			VARCHAR(14),
+					  RG			VARCHAR(12),
+					  Bairro		VARCHAR(50),
+					  Telefone		VARCHAR(11),
+					  Data_Nasc		VARCHAR(8),
+					  CEP		    VARCHAR(9),
+					  Endereco	    VARCHAR(255),
+					  Num_Endereco  INT,
+					  Cidade		VARCHAR(50),
+					  UF			VARCHAR(2),
+					  Complemento	VARCHAR(255),
+					  Nome			VARCHAR(50),
+					  Email			VARCHAR(50),
+					  bloqueado		VARCHAR(1))
+
+
+CREATE TABLE PEDIDO (IdPedido      INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+					 IdUsuario     INT FOREIGN KEY REFERENCES USUARIO(IdUsuario) NOT NULL,
+					 Num_Pedido    INT,
+					 IdCliente     INT FOREIGN KEY REFERENCES CLIENTE(IdCliente) NOT NULL)
+
+CREATE TABLE STATUSPEDIDO (IdSatusPedido VARCHAR(10) PRIMARY KEY,
+						   IdPedido      INT FOREIGN KEY REFERENCES PEDIDO(IdPedido) NOT NULL,
+						   Status_Pedido INT,
+						   Data_Pedido   INT,
+						   Data_Entrega  DATETIME);
+
---
+++ DB/Objetos/Cliente/SP_MANIPULA_CLIENTE.sql
@@ -0,0 +1,55 @@
+CREATE PROCEDURE dbo.SP_MANIPULA_CLIENTE 
+    @idCliente    INT,
+	@CPF          VARCHAR(14),
+	@RG           VARCHAR(12),
+	@Bairro       VARCHAR(50),
+	@Telefone     VARCHAR(11),
+	@Data_Nasc    VARCHAR(8),
+	@CEP          VARCHAR(9),
+	@Endereco     VARCHAR(255),
+	@Num_Endereco INT, 
+	@Cidade       VARCHAR(50),
+	@UF           VARCHAR(2),
+	@Complemento  VARCHAR(255),
+	@Nome         VARCHAR(50),
+	@Email        VARCHAR(50),
+	@bloqueado    VARCHAR(1),
+	@operacao     VARCHAR(1) 
+	
+	AS 
+	-- INSERT 
+	IF (@operacao = 'I')
+	BEGIN
+		INSERT INTO dbo.CLIENTE (CPF,RG,Bairro,Telefone,Data_Nasc,CEP,Endereco,Num_Endereco,Cidade,UF,Complemento,Nome,Email,bloqueado)
+		VALUES (@CPF,@RG,@Bairro,@Telefone,@Data_Nasc,@CEP,@Endereco,@Num_Endereco, @Cidade,@UF,@Complemento,@Nome,@Email,@bloqueado)
+	END
+
+
+	-- UPDATE 
+	IF (@operacao = 'U')
+	BEGIN
+		UPDATE  dbo.CLIENTE
+		SET	    CPF          = @CPF,
+			    RG           = @RG,
+			    Bairro       = @Bairro,
+			    Telefone     = @Telefone,
+			    Data_Nasc    = @Data_Nasc,
+			    CEP          = @CEP,
+			    Endereco     = @Endereco,
+			    Num_Endereco = @Num_Endereco, 
+			    Cidade       = @Cidade,
+			    UF           = @UF,
+			    Complemento  = @Complemento,
+			    Nome         = @Nome,
+			    Email        = @Email,
+			    bloqueado    = @bloqueado
+		WHERE IdCliente = @idCliente
+	END
+
+	-- DELETE 
+	IF (@operacao = 'D')
+	BEGIN
+		DELETE FROM dbo.CLIENTE
+		WHERE IdCliente = @idCliente
+	END
+
