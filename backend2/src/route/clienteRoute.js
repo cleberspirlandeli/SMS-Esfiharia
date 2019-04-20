a77d5111738327d036e0c7f79aa813602a69304f
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-04-20 14:03:21 -0300

    Routes
    Routes

--- backend2/src/route/clienteRoute.js
+++ backend2/src/route/clienteRoute.js
@@ -1,17 +1,35 @@
+// você criou as funções/metodos que estão no src/cliente/clienteController.js
+// agora você precisa importar essas funções/metodos aqui nesse arquivo
 const ClienteController = require('../cliente/ClienteController');
 
 module.exports = function (app) {
+    // SELECT 1 cliente
+    // Selecionar somente um cliente passando o ID por parametro
+    // Você precisa receber esse paramentro no req.params.idCliente
     app.route('/cliente/:idCliente')
         .get(ClienteController.listarCliente);
 
+    // SELECT TODOS clientes    
+    // Selecionar todos os clientes    
     app.route('/cliente/')
         .get(ClienteController.listarCliente);
-    // .post(ColaboradorCtrl.inserirColaborador);
+    
+    // INSERT
+    //Inserir um cliente    
+    //Você recebe as variaveis por req.body.NOME_DA_VARIAVEL
+    app.route('/cliente/')
+        .post(ClienteController.inserirCliente);
 
-    //     app.route('/api/colaborador/:id')
-    //         .get(ColaboradorCtrl.listarColaborador);
+    // UPDATE    
+    // Para fazer o UPDATE você precisa receber as variáveis por 2 lugares
+    // Recebe o ID por req.params.idCliente
+    // As outras informações recebe por req.body.NOME_DA_VARIAVEL 
+    app.route('/cliente/:idCliente')
+         .put(ClienteController.listarColaborador);
 
-    //     app.route('/api/colaborador/:idalteracao/:idcolaborador')
-    //         .delete(ColaboradorCtrl.excluirColaborador)
-    //         .put(ColaboradorCtrl.alterarColaborador);
+
+    // REMOVE
+    // Para remover o cliente recebe o ID por req.params.idCliente
+    app.route('/cliente/:idCliente')
+        .delete(ClienteController.removerCliente);
 }
